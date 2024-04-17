f6075a8592f786790cd8a770ae6244bc9327eb86
Felicia Hu <feliciahu1992_@hotmail.com>
2024-04-18 01:13:35 +0200

    Fix typos in about.tsx and intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -64,7 +64,7 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         >
-          <span className="font-bold">Hello, I'm Felicia.</span> I'm a{" "}
+          <span className="font-bold">Hello, I&apos;m Felicia.</span> I'm a{" "}
           <span className="font-bold">frontend student</span> with{" "}
           <span className="font-bold">1 year</span> of experience. I enjoy
           building <span className="italic">sites & apps</span>. My focus is{" "}
