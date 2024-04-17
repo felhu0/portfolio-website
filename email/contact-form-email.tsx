88ea766ef0ff05e8af496226f08dcd428fed5566
Felicia Hu <feliciahu1992_@hotmail.com>
2024-04-18 01:01:12 +0200

    first commit

---
+++ email/contact-form-email.tsx
@@ -0,0 +1,45 @@
+import {
+    Html,
+    Body,
+    Head,
+    Heading,
+    Hr,
+    Container,
+    Preview,
+    Section,
+    Text
+} from '@react-email/components'
+import { Tailwind } from '@react-email/components'
+
+type ContactFormEmailProps = {
+    message: string
+    senderEmail: string
+}
+
+export default function ContactFormEmail({
+    message,
+    senderEmail
+}: ContactFormEmailProps) {
+  return (
+    <Html>
+        <Head />    
+        <Preview>New message from your portfolio site </Preview>
+        <Tailwind>
+            <Body className='bg-gray-100 text-black'>
+                <Container>
+                    <Section className='bg-white border-black my-10 px-10 py-4 rounded-md'>
+                        <Text className='leading-tight '>
+                            You received the following message from the contact form
+                        </Text>
+                        <Text>{message}</Text>
+                        <Hr />
+                        <Text>
+                            The sender's email is: {senderEmail}
+                        </Text>
+                    </Section>
+                </Container>
+            </Body>
+        </Tailwind>
+    </Html>
+  )
+}
